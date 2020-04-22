b1a873aace9a78f2e01c64221e7bfa5bcc03bef7
CircleCI <mail@saschagrunert.de>
2020-04-22 10:25:43 +0000

    Update documentation

--- doc/implementors/core/cmp/trait.PartialOrd.js
+++ doc/implementors/core/cmp/trait.PartialOrd.js
@@ -1,10 +1,3 @@
 (function() {var implementors = {};
-implementors["indextree"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.PartialOrd.html\" title=\"trait core::cmp::PartialOrd\">PartialOrd</a>&lt;<a class=\"struct\" href=\"indextree/struct.NodeId.html\" title=\"struct indextree::NodeId\">NodeId</a>&gt; for <a class=\"struct\" href=\"indextree/struct.NodeId.html\" title=\"struct indextree::NodeId\">NodeId</a>",synthetic:false,types:["indextree::id::NodeId"]},];
-
-            if (window.register_implementors) {
-                window.register_implementors(implementors);
-            } else {
-                window.pending_implementors = implementors;
-            }
-        
-})()
+implementors["indextree"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.PartialOrd.html\" title=\"trait core::cmp::PartialOrd\">PartialOrd</a>&lt;<a class=\"struct\" href=\"indextree/struct.NodeId.html\" title=\"struct indextree::NodeId\">NodeId</a>&gt; for <a class=\"struct\" href=\"indextree/struct.NodeId.html\" title=\"struct indextree::NodeId\">NodeId</a>","synthetic":false,"types":["indextree::id::NodeId"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- doc/implementors/core/ops/index/trait.Index.js
+++ doc/implementors/core/ops/index/trait.Index.js
@@ -1,10 +1,3 @@
 (function() {var implementors = {};
-implementors["indextree"] = [{text:"impl&lt;T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/index/trait.Index.html\" title=\"trait core::ops::index::Index\">Index</a>&lt;<a class=\"struct\" href=\"indextree/struct.NodeId.html\" title=\"struct indextree::NodeId\">NodeId</a>&gt; for <a class=\"struct\" href=\"indextree/struct.Arena.html\" title=\"struct indextree::Arena\">Arena</a>&lt;T&gt;",synthetic:false,types:["indextree::arena::Arena"]},];
-
-            if (window.register_implementors) {
-                window.register_implementors(implementors);
-            } else {
-                window.pending_implementors = implementors;
-            }
-        
-})()
+implementors["indextree"] = [{"text":"impl&lt;T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/index/trait.Index.html\" title=\"trait core::ops::index::Index\">Index</a>&lt;<a class=\"struct\" href=\"indextree/struct.NodeId.html\" title=\"struct indextree::NodeId\">NodeId</a>&gt; for <a class=\"struct\" href=\"indextree/struct.Arena.html\" title=\"struct indextree::Arena\">Arena</a>&lt;T&gt;","synthetic":false,"types":["indextree::arena::Arena"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()

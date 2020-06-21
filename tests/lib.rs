2840ab7e6f5bbae3a77e4e1de40727695d8047ff
Teymour Aldridge <42674621+teymour-aldridge@users.noreply.github.com>
2020-06-21 17:11:15 +0100

    Add `get_node_id` function + test. (#60)
    Added a test.
    
    Reformat code.

--- tests/lib.rs
+++ tests/lib.rs
@@ -171,3 +171,20 @@ fn insert_removed_node() {
     assert!(n1.checked_insert_before(n2, &mut arena).is_err());
     assert!(n2.checked_insert_before(n1, &mut arena).is_err());
 }
+
+#[test]
+fn retrieve_node_id() {
+    let mut arena = Arena::new();
+    let n1_id = arena.new_node("1");
+    let n2_id = arena.new_node("2");
+    let n3_id = arena.new_node("3");
+    let n1 = arena.get(n1_id).unwrap();
+    let n2 = arena.get(n2_id).unwrap();
+    let n3 = arena.get(n3_id).unwrap();
+    let retrieved_n1_id = arena.get_node_id(n1).unwrap();
+    let retrieved_n2_id = arena.get_node_id(n2).unwrap();
+    let retrieved_n3_id = arena.get_node_id(n3).unwrap();
+    assert_eq!(retrieved_n1_id, n1_id);
+    assert_eq!(retrieved_n2_id, n2_id);
+    assert_eq!(retrieved_n3_id, n3_id);
+}

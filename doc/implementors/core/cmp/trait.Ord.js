85f504e58fd29038b975d259f7b239deef268888
CircleCI <mail@saschagrunert.de>
2021-04-30 07:39:55 +0000

    Update documentation

--- doc/implementors/core/cmp/trait.Ord.js
+++ doc/implementors/core/cmp/trait.Ord.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["indextree"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.Ord.html\" title=\"trait core::cmp::Ord\">Ord</a> for <a class=\"struct\" href=\"indextree/struct.NodeId.html\" title=\"struct indextree::NodeId\">NodeId</a>","synthetic":false,"types":["indextree::id::NodeId"]}];
+implementors["indextree"] = [{"text":"impl Ord for NodeId","synthetic":false,"types":[]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- doc/implementors/core/hash/trait.Hash.js
+++ doc/implementors/core/hash/trait.Hash.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["indextree"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"struct\" href=\"indextree/struct.NodeId.html\" title=\"struct indextree::NodeId\">NodeId</a>","synthetic":false,"types":["indextree::id::NodeId"]},{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"enum\" href=\"indextree/enum.NodeEdge.html\" title=\"enum indextree::NodeEdge\">NodeEdge</a>","synthetic":false,"types":["indextree::traverse::NodeEdge"]}];
+implementors["indextree"] = [{"text":"impl Hash for NodeId","synthetic":false,"types":[]},{"text":"impl Hash for NodeEdge","synthetic":false,"types":[]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()

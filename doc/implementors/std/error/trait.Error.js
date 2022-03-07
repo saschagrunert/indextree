815f1644366b82c9c006d6789537cede2fd6aaaf
CircleCI <mail@saschagrunert.de>
2022-03-07 08:39:37 +0000

    Update documentation

--- doc/implementors/std/error/trait.Error.js
+++ doc/implementors/std/error/trait.Error.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["indextree"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"enum\" href=\"indextree/enum.NodeError.html\" title=\"enum indextree::NodeError\">NodeError</a>","synthetic":false,"types":["indextree::error::NodeError"]}];
+implementors["indextree"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.59.0/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"enum\" href=\"indextree/enum.NodeError.html\" title=\"enum indextree::NodeError\">NodeError</a>","synthetic":false,"types":["indextree::error::NodeError"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()

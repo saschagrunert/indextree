99e5fe6fddb5cbe9822676a04e83dc585d1c55dc
CircleCI <mail@saschagrunert.de>
2023-01-09 09:02:01 +0000

    Update documentation

--- doc/source-files.js
+++ doc/source-files.js
@@ -1,3 +1,4 @@
-var sourcesIndex = {};
-sourcesIndex["indextree"] = {"name":"","files":["arena.rs","debug_pretty_print.rs","error.rs","id.rs","lib.rs","node.rs","relations.rs","siblings_range.rs","traverse.rs"]};
+var sourcesIndex = JSON.parse('{\
+"indextree":["",[],["arena.rs","debug_pretty_print.rs","error.rs","id.rs","lib.rs","node.rs","relations.rs","siblings_range.rs","traverse.rs"]]\
+}');
 createSourceSidebar();

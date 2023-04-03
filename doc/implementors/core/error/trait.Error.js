bb2b740429cdeacff0623914eb09009b4706fdf3
CircleCI <mail@saschagrunert.de>
2023-04-03 07:26:02 +0000

    Update documentation

--- doc/implementors/core/error/trait.Error.js
+++ doc/implementors/core/error/trait.Error.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"indextree":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.67.1/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"enum\" href=\"indextree/enum.NodeError.html\" title=\"enum indextree::NodeError\">NodeError</a>"]]
+"indextree":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.68.2/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"enum\" href=\"indextree/enum.NodeError.html\" title=\"enum indextree::NodeError\">NodeError</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()

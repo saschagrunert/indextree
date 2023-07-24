1441ba31acc17fce5db77bdee64260734789ab2e
CircleCI <mail@saschagrunert.de>
2023-07-24 06:42:14 +0000

    Update documentation

--- doc/implementors/core/cmp/trait.PartialOrd.js
+++ doc/implementors/core/cmp/trait.PartialOrd.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"indextree":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.68.2/core/cmp/trait.PartialOrd.html\" title=\"trait core::cmp::PartialOrd\">PartialOrd</a>&lt;<a class=\"struct\" href=\"indextree/struct.NodeId.html\" title=\"struct indextree::NodeId\">NodeId</a>&gt; for <a class=\"struct\" href=\"indextree/struct.NodeId.html\" title=\"struct indextree::NodeId\">NodeId</a>"]]
+"indextree":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.71.0/core/cmp/trait.PartialOrd.html\" title=\"trait core::cmp::PartialOrd\">PartialOrd</a>&lt;<a class=\"struct\" href=\"indextree/struct.NodeId.html\" title=\"struct indextree::NodeId\">NodeId</a>&gt; for <a class=\"struct\" href=\"indextree/struct.NodeId.html\" title=\"struct indextree::NodeId\">NodeId</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()

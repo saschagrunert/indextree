63ecd0172a6fb3ccd2d0df97165f72636bc17cdf
Alexei Mozaidze <lexerpexer@proton.me>
2024-07-20 01:29:36 +0400

    feat(macros): made `tree!` macro more ergonomic
    Allowed specifying both pre-existing nodes and literal nodes. It checks the types at runtime, but in a way that could *potentially* be optimized out by the compiler.
    
    Additionally, improved ergonomics of `tree!` by making the root node part of the tree representation. (`tree!(arena, root_node, ...nodes)` => `tree!(arena, root_node => { ...nodes })`)

--- indextree/examples/tree-macro.rs
+++ indextree/examples/tree-macro.rs
@@ -2,15 +2,35 @@ use indextree::{Arena, macros::tree};
 
 fn main() {
     let mut arena = Arena::new();
-    let root_node = arena.new_node("my root node");
 
-    tree! {
+    // It works with existing nodes
+    let root_node = arena.new_node("my root node");
+    tree!(
         &mut arena,
-        root_node,
-        "1",
-        "2" => { "2_1" },
-        "3"
-    };
+        root_node => {
+            "1",
+            "2" => {
+                "2_1" => { "2_1_1" },
+                "2_2",
+            },
+            "3" => {},
+        }
+    );
+
+    println!("{}", root_node.debug_pretty_print(&arena));
+
+    // It can also create a root node for you!
+    let root_node = tree!(
+        &mut arena,
+        "my root node, but automagically created" => {
+            "1",
+            "2" => {
+                "2_1" => { "2_1_1" },
+                "2_2",
+            },
+            "3",
+        }
+    );
 
     println!("{}", root_node.debug_pretty_print(&arena));
 }

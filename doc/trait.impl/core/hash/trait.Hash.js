ac105867a77e403e15a5e2105bd03674040d94ea
CircleCI <mail@saschagrunert.de>
2025-05-07 13:32:29 +0000

    Update documentation

--- doc/trait.impl/core/hash/trait.Hash.js
+++ doc/trait.impl/core/hash/trait.Hash.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["indextree",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.1/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"enum\" href=\"indextree/enum.NodeEdge.html\" title=\"enum indextree::NodeEdge\">NodeEdge</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.1/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"struct\" href=\"indextree/struct.NodeId.html\" title=\"struct indextree::NodeId\">NodeId</a>"]]]]);
+    var implementors = Object.fromEntries([["indextree",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.86.0/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"enum\" href=\"indextree/enum.NodeEdge.html\" title=\"enum indextree::NodeEdge\">NodeEdge</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.86.0/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"struct\" href=\"indextree/struct.NodeId.html\" title=\"struct indextree::NodeId\">NodeId</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
